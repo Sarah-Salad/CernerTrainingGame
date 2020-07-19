e16d5188bbb60f5b419dc05dc72f58e3ffcf5bd7
SarahSalad <selester12@gmail.com>
2020-07-18 19:50:16 -0500

    fixed main menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,6 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+  private void Awake() {
+    Cursor.lockState = CursorLockMode.None;
+    Cursor.visible = true;
+  }
+
   public void PlayGame()
   {
     SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
